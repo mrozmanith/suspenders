6c5cc40fbb6f8a52bf5358baa2c2d9c3c62fab52
Mike Burns <mburns@thoughtbot.com>
2017-12-31 01:37:03 -0500

    Style

--- lib/suspenders/generators/ci_generator.rb
+++ lib/suspenders/generators/ci_generator.rb
@@ -4,10 +4,12 @@ module Suspenders
   class CiGenerator < Rails::Generators::Base
     source_root File.expand_path(
       File.join("..", "..", "..", "templates"),
-      File.dirname(__FILE__))
+      File.dirname(__FILE__)
+    )
 
     def simplecov_test_integration
-      inject_into_file "spec/spec_helper.rb", before: 'SimpleCov.start "rails"' do
+      start_simple_cov = 'SimpleCov.start "rails"'
+      inject_into_file "spec/spec_helper.rb", before: start_simple_cov do
         <<-RUBY
 
   if ENV["CIRCLE_ARTIFACTS"]
